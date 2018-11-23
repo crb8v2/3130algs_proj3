8d7df8358ebf992f3f79a9cddeb9a7ca78903e21
Connor <connor@admin.com>
2018-11-23 13:15:52 -0600

    init

---
+++ cmake-build-debug/CMakeFiles/BANC3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BANC3.dir/main.c.o"
+  "BANC3.pdb"
+  "BANC3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BANC3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
